4d2d51c2144f16b1ce3737dd284246e9107ff123
Code06 <47684894+maddosaientisuto@users.noreply.github.com>
2021-03-25 23:03:01 +0500

    Finished train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,41 +1,122 @@
+# import libraries
 import sys
-
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+import re
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+from sqlalchemy import create_engine
+import pickle
+from scipy.stats import gmean
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
 
 def load_data(database_filepath):
-    pass
+    """Loads the filepath and returns the data"""
+    name = 'sqlite:///' + database_filepath
+    engine = create_engine(name)
+    df = pd.read_sql_table('Disasters', con=engine)
+    print(df.head())
+    # Extract X and y variables from the data for the modelling
+    X = df['message']
+    y = df[df.columns[5:]]
+    added = pd.get_dummies(df[['related','genre']])
+    y = pd.concat([y, added], axis=1)
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """tokenize and transform input text. Return cleaned text.
+    Arguments:
+        text -> Text message which needs to be tokenized
+    Output:
+        clean_tokens -> List of tokens extracted from the provided text
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    # Extract all the urls from the provided text
+    detected_urls = re.findall(url_regex, text)
+    # Replace url with a url placeholder string
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """Return Grid Search model with pipeline and Classifier"""
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ]))
+            
+        ])),
+
+        ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    parameters = {'classifier__estimator__learning_rate': [0.01, 0.02, 0.05],
+              'classifier__estimator__n_estimators': [10, 20, 40]}
+
+    cv = GridSearchCV(pipeline, parameters)
+
+    return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    """Print model results
+    INPUT
+    model -- required, estimator-object
+    X_test -- required
+    y_test -- required
+    category_names = required, list of category strings
+    OUTPUT
+    None
+    """
+    # Get results and add them to a dataframe
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
+    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
 
 
 def save_model(model, model_filepath):
-    pass
+    """Save model as pickle file"""
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
+    """Load the data, run the model and save model"""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
